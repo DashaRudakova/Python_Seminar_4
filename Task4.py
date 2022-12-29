4d41876e2d145afa6dccd16139cd234575ab6568
DashaRudakova <fluffy575@mail.ru>
2022-12-29 19:31:01 +0300

    4

---
+++ Task4.py
@@ -0,0 +1,16 @@
+# Задана натуральная степень k. 
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# 
+# Пример:
+# 
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+from random import randint
+
+
+k = int(input('Введите число -> '))
+a = randint(0,100)
+b = randint(0,100)
+c = randint(0,100)
+with open('C:\Users\darud\Desktop\python\Seminar4', 'w', encoding='utf-8') as file:
+   file.write(f'{a}*x^{k} + {b}*x + {c} = 0')
